a820db33da9c01ab35d57a3083582c45fa4fe59f
Laurenz Illner <l.illner@tu-bs.de>
2023-09-22 14:38:44 +0200

    documentation memetic algorithm

--- mem.cpp
+++ mem.cpp
@@ -3,11 +3,21 @@
 #include <random>
 #include <iostream>
 
+/**
+ * logger function for benchmarking
+ * @param makespan current best makespan
+ */
 void MemeticAlgorithm::logMakespan(int makespan) {
     std::chrono::duration<double> elapsed_seconds = (std::chrono::system_clock::now() - tStart);
     makespanHistory.emplace_back(std::tuple{elapsed_seconds.count(), makespan});
 }
 
+/**
+ * main function, initialize with object with an JSSPInstance, start optimization/benchmark with this function
+ * @param time_limit maximum runtime - soft limit
+ * @param known_optimum or lb for an early stop
+ * @return struct: solution, value, log
+ */
 BMResult MemeticAlgorithm::optimize(int time_limit, int known_optimum) {
     tStart = std::chrono::system_clock::now();
     makespanHistory = vector<std::tuple<double,int>>();
@@ -31,6 +41,14 @@ BMResult MemeticAlgorithm::optimize(int time_limit, int known_optimum) {
     return BMResult{currentBest.solution, currentBest.makespan, makespanHistory};
 }
 
+/**
+ * secondary main function, initialize with object with an JSSPInstance, start optimization/benchmark with this function.
+ * use if you want to start with a set start solutions, else use MemeticAlgorithm::optimize
+ * @param time_limit maximum runtime - soft limit
+ * @param known_optimum or lb for an early stop
+ * @param start_solutions vector of feasible start solutions
+ * @return struct: solution, value, log
+ */
 BMResult MemeticAlgorithm::optimizePopulation(int time_limit, vector<Solution> &start_solutions, int known_optimum) {
     tStart = std::chrono::system_clock::now();
     makespanHistory = vector<std::tuple<double,int>>();
@@ -51,6 +69,13 @@ BMResult MemeticAlgorithm::optimizePopulation(int time_limit, vector<Solution> &
 
 }
 
+/**
+ * internal function, the main loop of the algorithm
+ * function manipulates population and currentBest
+ * @param time_limit passed by optimize
+ * @param known_optimum passed by optimize
+ * @param ts_algo initialized tabu search instance
+ */
 void MemeticAlgorithm::optimizeLoop(int time_limit, int known_optimum, TabuSearch ts_algo) {
     rng = std::mt19937(instance.getSeed());
     std::uniform_int_distribution<std::mt19937::result_type> dist(0,populationSize - 1);
@@ -82,12 +107,22 @@ void MemeticAlgorithm::optimizeLoop(int time_limit, int known_optimum, TabuSearc
     }
 }
 
+/**
+ * called by optimize, before starting main loop
+ */
 void MemeticAlgorithm::initializeRandPopulation() {
     while (population.size() < populationSize) {
         population.emplace_back(instance.generateRandomSolution());
     }
 }
 
+/**
+ * recombination operator: picks two parent solutions from population, finds longest common sequence (lcs) for each
+ * machine and calls crossover on each machine pair to create two new child machines
+ * @param parent_1
+ * @param parent_2
+ * @return two new child solutions, with both parents features
+ */
 std::tuple<Solution, Solution> MemeticAlgorithm::recombinationOperator(Solution const &parent_1, Solution const &parent_2) {
     auto child_1_solution = vector<vector<int>>();
     auto child_2_solution = vector<vector<int>>();
@@ -101,6 +136,18 @@ std::tuple<Solution, Solution> MemeticAlgorithm::recombinationOperator(Solution 
     return {Solution{child_1_solution, makespan_1}, Solution{child_2_solution, makespan_2}};
 }
 
+/**
+ * the crossover operator (1) copies the longest common sequence (lcs) of one parent (p) in one child (c) and
+ * (2) fills the gaps in the child machine with the operations of the other parent, in the order they appear
+ * Example:
+ * p1 : [1 2 3 4 5 6], p2 : [3 2 4 1 5 6]
+ * c1 : [1       5 6], c2 : [      1 5 6] (1)
+ * c1 : [1 3 2 4 5 6], c2 : [2 3 4 1 5 6] (2)
+ * @param machine_parent_1
+ * @param machine_parent_2
+ * @param lcs longest common sequence of both parents
+ * @return two child machine sequences as described above
+ */
 vector<int> MemeticAlgorithm::crossover(vector<int> const &machine_parent_1, vector<int> const &machine_parent_2, vector<int> const &lcs) {
     vector<int> p2_without_lcs = vector<int>();
     int i_lcs = 0;
@@ -126,6 +173,15 @@ vector<int> MemeticAlgorithm::crossover(vector<int> const &machine_parent_1, vec
     return child_sequence;
 }
 
+/**
+ * finds the longest common sequence (lcs) of two given (parent) machines with dynamic programming (-> dp_matrix)
+ * Example:
+ * m1 : [1 2 3 4 5 6], m2 : [5 1 6 2 3 4]
+ * lcs : [1 2 3 4]
+ * @param machine_1
+ * @param machine_2
+ * @return the longest common sequence of the two machines
+ */
 vector<int> MemeticAlgorithm::findLongestCommonSequence(const vector<int> &machine_1, const vector<int> &machine_2) {
     auto len = machine_1.size() + 1;
     auto dp_matrix = vector<vector<int>>();
@@ -161,6 +217,13 @@ vector<int> MemeticAlgorithm::findLongestCommonSequence(const vector<int> &machi
     return lcs;
 }
 
+/**
+ * rates the solution at given index by similarity to all other solutions of the population
+ * similarity: maximum in the population of the sum over the length of all longest common sequences (lcs) for each
+ * same machine pair
+ * @param solution_index index of the solution in the population
+ * @return similarity degree
+ */
 int MemeticAlgorithm::calcSimilarityDegree(int solution_index) {
     auto similarity_degree = vector<int>();
     for (int i = 0; i < population.size(); i++) {
@@ -176,6 +239,10 @@ int MemeticAlgorithm::calcSimilarityDegree(int solution_index) {
     return *std::max_element(similarity_degree.begin(), similarity_degree.end());
 }
 
+/**
+ * calcs quality and similarity score for each solution and removes the worst two.
+ * population size at start of the method is populationSize + 2, at the end populationSize
+ */
 void MemeticAlgorithm::updatePopulation() {
     vector<std::tuple<float,int>> quality_list = vector<std::tuple<float,int>>();
     vector<int> similarity_degrees = vector<int>();
@@ -202,6 +269,11 @@ void MemeticAlgorithm::updatePopulation() {
     }
 }
 
+/**
+ * optimization with iteration limit, mainly for testing, no logging
+ * @param max_iterations
+ * @return best solution
+ */
 Solution MemeticAlgorithm::optimizeIterationConstraint(int max_iterations) {
     int current_iteration = 0;
     population = vector<Solution>();
